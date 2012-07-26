ba1186c47e5f3466faeea9f2d5bf96948d5f7183
Jamie Matthews <jamie.matthews@gmail.com>
2012-07-26 12:18:29 +0100

    Add --strict flag to raise exception on undefined variables

--- confuzzle.py
+++ confuzzle.py
@@ -1,11 +1,13 @@
 import sys
 import argparse
 import yaml
-from jinja2 import Template
+import jinja2
 
 
-def render(template_string, context_dict):
-    template = Template(template_string)
+def render(template_string, context_dict, strict=False):
+    template = jinja2.Template(template_string)
+    if strict:
+        template.environment.undefined = jinja2.StrictUndefined
     return template.render(**context_dict)
 
 
@@ -15,13 +17,14 @@ def main():
     parser.add_argument('template', nargs='?', type=argparse.FileType('r'), default=sys.stdin, help="Config file template. If not supplied, stdin is used")
     parser.add_argument('config', type=argparse.FileType('r'), help="YAML data file to read")
     parser.add_argument('--out', '-o', dest='out', type=argparse.FileType('w'), default=sys.stdout, help="Output file to write. If not supplied, stdout is used")
+    parser.add_argument('--strict', dest='strict', action='store_true', default=False, help="Raise an exception on undefined variables")
 
     args = parser.parse_args()
 
     context_dict = yaml.load(args.config.read())
     template_string = args.template.read()
 
-    rendered = render(template_string, context_dict)
+    rendered = render(template_string, context_dict, args.strict)
     args.out.write(rendered)
 
 
