9f31fc331fa28f005461ac62631dfdec559fa6b0
Jamie Matthews <jamie.matthews@gmail.com>
2012-07-26 11:45:17 +0100

    Add license and setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='confuzzle',
+    author='Jamie Matthews',
+    author_email='jamie.matthews@gmail.com',
+    version='0.1.0',
+    url='http://github.com/j4mie/confuzzle',
+    py_modules=['confuzzle'],
+    description='A tiny tool for generating templated config files',
+    license='Public Domain',
+    entry_points={
+        'console_scripts': [
+            'confuzzle = confuzzle:main'
+        ]
+    },
+    classifiers=[
+        'Programming Language :: Python'
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: Public Domain',
+    ],
+)
