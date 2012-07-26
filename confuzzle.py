7d066a9f4d66d96ab3e888a870cd3cb4dc46efa8
Jamie Matthews <jamie.matthews@gmail.com>
2012-07-26 14:04:58 +0100

    Template file option is now a flag: --template-file

--- confuzzle.py
+++ confuzzle.py
@@ -14,8 +14,8 @@ def render(template_string, context_dict, strict=False):
 def main():
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('template', nargs='?', type=argparse.FileType('r'), default=sys.stdin, help="Config file template. If not supplied, stdin is used")
     parser.add_argument('config', type=argparse.FileType('r'), help="YAML data file to read")
+    parser.add_argument('--template-file', '-f', dest='template', type=argparse.FileType('r'), default=sys.stdin, help="Config file template. If not supplied, stdin is used")
     parser.add_argument('--out', '-o', dest='out', type=argparse.FileType('w'), default=sys.stdout, help="Output file to write. If not supplied, stdout is used")
     parser.add_argument('--strict', dest='strict', action='store_true', default=False, help="Raise an exception on undefined variables")
 
